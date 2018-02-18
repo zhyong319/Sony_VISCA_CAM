e881c384fb4965e06ab167927874c62852792fdc
Home PC <>
2018-02-18 23:16:22 +0800

    Sony VISCA camera control app, this is a win32 app

---
+++ VISCA_CAM/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace VISCA_CAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(157, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6"});
+            this.comboBox1.Location = new System.Drawing.Point(64, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(73, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "串口";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Location = new System.Drawing.Point(10, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 78);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口配置";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Controls.Add(this.button11);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(10, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 225);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "控制";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(278, 135);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(58, 30);
+            this.button12.TabIndex = 8;
+            this.button12.Text = "停止";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(213, 45);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(166, 29);
+            this.button11.TabIndex = 7;
+            this.button11.Text = "获取云台当前位置数据";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(318, 168);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(61, 30);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "推远";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(237, 168);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(61, 30);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "拉近";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(318, 91);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(61, 30);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "电源关";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(237, 91);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(61, 30);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "电源开";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(116, 131);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 37);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "右";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(67, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "下";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(67, 131);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 37);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "停止";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "左";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "上";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(133, 46);
+            this.textBox1.TabIndex = 5;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.ReadTimeout = 2000;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.OnRevData);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Location = new System.Drawing.Point(249, 11);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(160, 78);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "数据接收";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(54, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "云台步进操作使能";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(54, 52);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(120, 16);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "镜头步进操作使能";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::VISCA_CAM.Properties.Resources.tim1g;
+            this.ClientSize = new System.Drawing.Size(425, 342);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Sony VISCA控制协议";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ VISCA_CAM/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace VISCA_CAM
+{
+    public partial class Form1 : Form
+    {
+        //byte[] revBuff;
+        public Form1()
+        {
+            InitializeComponent();
+            this.comboBox1.Text = @"COM3";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //打开串口
+        {
+           
+            
+            if (this.serialPort1.IsOpen == false) 
+            {
+                try
+                {
+                    this.serialPort1.StopBits = StopBits.One;
+                    this.serialPort1.DataBits = 8;
+                    this.serialPort1.BaudRate = 9600;
+                    this.serialPort1.PortName = this.comboBox1.Text;
+                    this.serialPort1.Open();
+                    this.button1.Text = "关闭串口";
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("访问被拒绝的端口！");
+                }
+                catch (InvalidOperationException)
+                {
+                    MessageBox.Show("指定的端口已被打开！");
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("该端口处于无效状态");
+                }
+            }
+            else
+            {
+                this.serialPort1.Close();
+                this.button1.Text = "打开串口";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) // up
+        {
+            byte[] buff_up = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05,0x03,0x01,0xff };
+            this.serialPort1.Write(buff_up, 0, buff_up.Length);
+            if (this.checkBox1.Checked == true)
+            {
+                byte[] buff_stop = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05, 0x03, 0x03, 0xff };
+                this.serialPort1.Write(buff_stop, 0, buff_stop.Length);
+            }
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button4_Click(object sender, EventArgs e) //down
+        {
+            byte[] buff_down = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05, 0x03, 0x02, 0xff };
+            this.serialPort1.Write(buff_down, 0, buff_down.Length);
+            if (this.checkBox1.Checked == true)
+            {
+                byte[] buff_stop = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05, 0x03, 0x03, 0xff };
+                this.serialPort1.Write(buff_stop, 0, buff_stop.Length);
+            }
+            this.textBox1.Text = "数据发送成功";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) //left
+        {
+            byte[] buff_left = { 0x81,0x01,0x06,0x01,0x05,0x00,0x01,0x03,0xff};
+            this.serialPort1.Write(buff_left, 0, buff_left.Length);
+            if (this.checkBox1.Checked == true)
+            {
+                byte[] buff_stop = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05, 0x03, 0x03, 0xff };
+                this.serialPort1.Write(buff_stop, 0, buff_stop.Length);
+            }
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button5_Click(object sender, EventArgs e) //right
+        {
+            byte[] buff_right = { 0x81, 0x01, 0x06, 0x01, 0x05, 0x00, 0x02, 0x03, 0xff };
+            this.serialPort1.Write(buff_right, 0, buff_right.Length);
+            if (this.checkBox1.Checked == true)
+            {
+                byte[] buff_stop = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05, 0x03, 0x03, 0xff };
+                this.serialPort1.Write(buff_stop, 0, buff_stop.Length);
+            }
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button6_Click(object sender, EventArgs e) //stop
+        {
+            byte[] buff_stop = { 0x81, 0x01, 0x06, 0x01, 0x00, 0x05, 0x03, 0x03, 0xff };
+            this.serialPort1.Write(buff_stop, 0, buff_stop.Length);
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button7_Click(object sender, EventArgs e) //power on
+        {
+            byte[] buff_poweron = { 0x81,0x01,0x04,0x00,0x02,0xff};
+            this.serialPort1.Write(buff_poweron,0,buff_poweron.Length);
+            this.textBox1.Text = "数据发送成功";
+
+        }
+
+        private void button8_Click(object sender, EventArgs e) //pwoer off
+        {
+            byte[] buff_poweroff = { 0x81, 0x01, 0x04, 0x00, 0x03, 0xff };
+            this.serialPort1.Write(buff_poweroff, 0, buff_poweroff.Length);
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button11_Click(object sender, EventArgs e) //get local infomation
+        {
+            byte[] buff = { 0x81,0x09,0x06,0x12,0xff};
+            this.serialPort1.Write(buff, 0, buff.Length);
+            this.textBox1.Text = "数据发送成功";
+           
+                    
+        }
+
+        private void OnRevData(object sender, SerialDataReceivedEventArgs e) //rev serial data
+        {
+            byte[] inputBuff = new byte[20];
+            try
+            {
+                for (int ii = 0; ii < 20; ii++)
+                {
+
+                    inputBuff[ii] = (byte)this.serialPort1.ReadByte();
+                    if (inputBuff[ii] == 0xff)
+                    {
+                        this.serialPort1.DiscardInBuffer();
+                        this.textBox1.Text = "数据接收成功！";
+                        this.textBox1.Text = byteToHexStr(inputBuff);
+                        return;
+                    }
+                }
+            }
+            catch (TimeoutException)
+            {
+                this.textBox1.Text = "数据接收超时!";
+            }
+        }
+
+        public static string byteToHexStr(byte[] bytes) //将BYTE数组转为16进制的字符串
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    returnStr += bytes[i].ToString("X2");
+                    if (bytes[i] == 0xff) return returnStr;
+                    returnStr += " ";
+                }
+            }
+            return returnStr;
+        }
+
+        private void button12_Click(object sender, EventArgs e) //镜头停止
+        {
+            byte[] buff = { 0x81,0x01,0x04,0x07,0x00,0xff };
+            this.serialPort1.Write(buff, 0, buff.Length);
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button9_Click(object sender, EventArgs e) //镜头拉近
+        {
+            byte[] buff = { 0x81, 0x01, 0x04, 0x07, 0x02, 0xff };
+            this.serialPort1.Write(buff, 0, buff.Length);
+            if (this.checkBox2.Checked == true)
+            {
+                byte[] buff1 = { 0x81, 0x01, 0x04, 0x07, 0x00, 0xff };
+                this.serialPort1.Write(buff1, 0, buff1.Length);
+            }
+            this.textBox1.Text = "数据发送成功";
+        }
+
+        private void button10_Click(object sender, EventArgs e) //镜头推远
+        {
+            byte[] buff = { 0x81, 0x01, 0x04, 0x07, 0x03, 0xff };
+            this.serialPort1.Write(buff, 0, buff.Length);
+            if (this.checkBox2.Checked == true)
+            {
+                byte[] buff1 = { 0x81, 0x01, 0x04, 0x07, 0x00, 0xff };
+                this.serialPort1.Write(buff1, 0, buff1.Length);
+            }
+            this.textBox1.Text = "数据发送成功";
+        }
+    }
+    }
+
+ 
